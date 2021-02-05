f3cad2e8cd18249d99cda32821492b391fac5994
Jonathan Soukaseume <sksmejn@gmail.com>
2021-02-04 21:47:27 -0800

    changed border of buttons to <hr> instead. added some js for testing now

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,17 @@
 console.log("Hello");
+
+// Upon About Container click
+$(".about-title").click(function() {
+    console.log("about-title clicked");
+    $(".about-container").text("Hello");
+})
+
+// Upon Project Container click
+$(".project-title").click(function() {
+    console.log("project-title clicked");
+})
+
+// Upon Conact Container click
+$(".contact-title").click(function() {
+    console.log("contact-title clicked");
+})
